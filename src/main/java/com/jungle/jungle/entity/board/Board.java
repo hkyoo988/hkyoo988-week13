07d8e1ce8d2b268785f49dd2eca4a5ddc076e8f3
hkyoo <hkyoo988@naver.com>
2024-06-07 22:02:32 +0900

    start

---
+++ src/main/java/com/jungle/jungle/entity/board/Board.java
@@ -0,0 +1,44 @@
+package com.jungle.jungle.entity.board;
+
+import com.jungle.jungle.dto.BoardRequestDto;
+import com.jungle.jungle.entity.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public void update(BoardRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.author = requestDto.getAuthor();
+        this.password = requestDto.getPassword();
+    }
+
+}
